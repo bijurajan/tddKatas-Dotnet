1e6e774bf001d2427346da61b0d5bdf9d20a81d4
Biju Rajan <brajan@Bijus-MacBook-Pro.local>
2017-09-29 11:07:15 -0400

    Adding in the banking-kata.WIP. Adding in the models and a dummy controller

---
+++ BankingKata/BankingKataAPI/Models/ICurrencyConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BankingKataAPI.Models
+{
+	public interface ICurrencyConverter
+	{
+		double GetRate(CurrencyCodes fromCode, CurrencyCodes toCode, DateTime asOn);
+		double GetRate(string fromCode, string toCode, DateTime asOn);
+	}
+}
